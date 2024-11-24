04085c256b08367a92c2487e16cb15c189dca495
AntonioMCGomes <marxmarc@gmail.com>
2024-11-24 12:34:05 +0000

    Loops

---
+++ Java Script/JS Guanabara/aula13/ambiente.js
@@ -0,0 +1,23 @@
+
+/*
+Loop com while.
+Primeiro houve um teste lógico e depois a execução do bloco.
+
+var c = 1
+while (c < 6){
+    console.log(`Oi, tudo bem?`)
+    c ++
+}
+
+*/
+
+/*
+Loop com do
+Primeiro há a execução do bloco e depois, um teste lógico.
+
+*/
+var c = 1
+do {
+    console.log(`Oi, tudo bem?`)
+    c++
+} while (c <= 6)
