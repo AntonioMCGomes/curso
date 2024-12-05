ea4bf32c09fa2997a14beccf335177b7a997ad05
AntonioMCGomes <marxmarc@gmail.com>
2024-12-05 19:37:43 +0000

    Attachments

---
+++ Java Script/JS Guanabara/aula17/Ex.js
@@ -0,0 +1,21 @@
+
+/*
+let obj = {}
+let lista = []
+function Usuario(){
+    let nomeUsuario = new Object(document.getElementById('nomeU').value)
+    let res = document.getElementById('res')
+    obj.nome = nomeUsuario
+    res.innerHTML = obj
+}
+*/
+
+let obj = {}
+let lista = []
+let form = new Object()
+obj.nome = "Antonio"
+obj.idade = 39
+obj.origem = 'Brasil'
+lista.push(obj)
+console.log(lista)
+
