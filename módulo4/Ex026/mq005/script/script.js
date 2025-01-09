810b7bb78288b25882772a3fe05bf41eb35999d7
AntonioMCGomes <marxmarc@gmail.com>
2025-01-09 12:17:07 +0000

    EX026 (mq005 update)

--- módulo4/Ex026/mq005/script/script.js
+++ módulo4/Ex026/mq005/script/script.js
@@ -1,18 +1,19 @@
-let menuImagem = document.getElementById('menu');
+/*let menuToogle = document.getElementById('menuToogle');
 let menuList = document.getElementById('menuList');
 
-menuImagem.addEventListener('click', () => {
-    if(menuList.style.display == 'block') {
+menuToogle.addEventListener('click', () => {
+    if(menuList.style.display === 'block') {
         menuList.style.display = 'none'
     } else{
         menuList.style.display = 'block'
     }
 })
 
-menuImagem.addEventListener('click', (event) => {
+menuToogle.addEventListener('click', (event) => {
     if(event.target.Tagname === 'A'){
-        menuList.style.display === 'none'
+        menuList.style.display = 'none'
     } 
 })
 
+*/
 
